3de88f261fcbff3871bee0dbb72fe6dc41b31472
aeskerm <kerminenartturi@gmail.com>
2024-07-29 11:03:21 +0300

    add cors

--- app/app.js
+++ app/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
+
 const dataRouter = require("./controllers/dataRouter");
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3003" }));
 
 app.use("/api/data", dataRouter);
 
